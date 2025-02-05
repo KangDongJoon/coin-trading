d39101b8dce7e563764f6ad6f2bf96221ded0183
KangDongJoon <ehdwns1119@naver.com>
2025-02-05 09:01:15 +0900

    [FIX] 계좌 조회 API Service분리

--- src/main/java/coin/cointrading/controller/TradingController.java
+++ src/main/java/coin/cointrading/controller/TradingController.java
@@ -2,16 +2,11 @@ package coin.cointrading.controller;
 
 import coin.cointrading.dto.AccountResponse;
 import coin.cointrading.service.TradingService;
-import coin.cointrading.util.JwtTokenProvider;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -20,26 +15,13 @@ import java.util.List;
 public class TradingController {
 
     private final TradingService tradingService;
-    private final JwtTokenProvider jwtTokenProvider;
-    private final RestTemplate restTemplate;
-    private final String serverUrl = "https://api.upbit.com";
 
-    @GetMapping("/v1/account")
+    @GetMapping("/v1/accounts")
     @Operation(summary = "계좌조회", description = "내 계좌 조회")
-    public List<AccountResponse> getaccount() {
-        String accountUrl = serverUrl + "/v1/accounts";
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        headers.set("Authorization", jwtTokenProvider.createToken());
-        HttpEntity<?> entity = new HttpEntity<>(headers);
-
-        return restTemplate.exchange(
-                accountUrl,
-                HttpMethod.GET,
-                entity,
-                new ParameterizedTypeReference<List<AccountResponse>>() {
-        }).getBody();
+    public ResponseEntity<List<AccountResponse>> getAccount() {
+        return ResponseEntity.ok(tradingService.getAccount());
     }
+
 }
 
 
--- src/main/java/coin/cointrading/service/TradingService.java
+++ src/main/java/coin/cointrading/service/TradingService.java
@@ -1,9 +1,38 @@
 package coin.cointrading.service;
 
+import coin.cointrading.dto.AccountResponse;
+import coin.cointrading.util.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class TradingService {
 
+    private final String serverUrl = "https://api.upbit.com";
+    private final JwtTokenProvider jwtTokenProvider;
+    private final RestTemplate restTemplate;
+
+    public List<AccountResponse> getAccount() {
+        String accountUrl = serverUrl + "/v1/accounts";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Authorization", jwtTokenProvider.createToken());
+        HttpEntity<?> entity = new HttpEntity<>(headers);
+
+        return restTemplate.exchange(
+                accountUrl,
+                HttpMethod.GET,
+                entity,
+                new ParameterizedTypeReference<List<AccountResponse>>() {
+                }).getBody();
+    }
 
 }
