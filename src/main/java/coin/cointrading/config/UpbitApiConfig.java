7d57d6ed4a8fd6de36396bd3b4a1d9077e088f0d
KangDongJoon <ehdwns1119@naver.com>
2025-02-06 06:43:17 +0900

    [FIX] 계좌 조회 API Service분리

--- src/main/java/coin/cointrading/config/UpbitApiConfig.java
+++ src/main/java/coin/cointrading/config/UpbitApiConfig.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class UpBitApiConfig {
+public class UpbitApiConfig {
     @Bean
     public RestTemplate getRestTemplate() {
         RestTemplateBuilder restTemplateBuilder = new RestTemplateBuilder();
