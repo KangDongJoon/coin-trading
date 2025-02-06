ec918b200d4ae6859713f2c8065f0923dc2624fd
KangDongJoon <ehdwns1119@naver.com>
2025-02-06 11:05:02 +0900

    [FEAT] 화폐 주문(매수/매도)기능

--- src/main/java/coin/cointrading/service/TradingService.java
+++ src/main/java/coin/cointrading/service/TradingService.java
@@ -1,7 +1,9 @@
 package coin.cointrading.service;
 
 import coin.cointrading.dto.AccountResponse;
+import coin.cointrading.dto.OrderResponse;
 import coin.cointrading.util.JwtTokenProvider;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -10,6 +12,9 @@ import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+import java.util.HashMap;
 import java.util.List;
 
 @Service
@@ -24,7 +29,7 @@ public class TradingService {
         String accountUrl = serverUrl + "/v1/accounts";
         HttpHeaders headers = new HttpHeaders();
         headers.set("Content-Type", "application/json");
-        headers.set("Authorization", jwtTokenProvider.createToken());
+        headers.set("Authorization", jwtTokenProvider.createAccountToken());
         HttpEntity<?> entity = new HttpEntity<>(headers);
 
         return restTemplate.exchange(
@@ -35,4 +40,35 @@ public class TradingService {
                 }).getBody();
     }
 
+    public OrderResponse orderCoin() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        // 계좌 및 gpt와 연동하여 결정
+        String side = "";
+        String price = "";
+        String volume = "";
+        String ord_type = side.equals("bid") ? "price" : "market";
+
+        HashMap<String, String> params = new HashMap<>();
+        params.put("market", "KRW-ETH");
+        params.put("side", side);
+        params.put("ord_type", ord_type);
+
+        if(side.equals("bid")){
+            params.put("price", price);
+        }else{
+            params.put("volume", volume);
+        }
+
+        String orderUrl = serverUrl + "/v1/orders";
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Content-Type", "application/json");
+        headers.set("Authorization", jwtTokenProvider.createOrderToken(params));
+        HttpEntity<String> entity = new HttpEntity<>(new Gson().toJson(params), headers);
+
+        return restTemplate.exchange(
+                orderUrl,
+                HttpMethod.POST,
+                entity,
+                new ParameterizedTypeReference<OrderResponse>() {
+                }).getBody();
+    }
 }
