dbd53bf4982895bc80e9080eec9d0047e85a2175
KangDongJoon <ehdwns1119@naver.com>
2025-02-08 22:01:23 +0900

    [FEAT] 변동성 돌파 전략 매매법

--- src/main/java/coin/cointrading/service/TradingService.java
+++ src/main/java/coin/cointrading/service/TradingService.java
@@ -8,12 +8,11 @@ import coin.cointrading.util.JwtTokenProvider;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.node.ArrayNode;
-import com.fasterxml.jackson.databind.node.ObjectNode;
 import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
-import okhttp3.*;
-import org.jetbrains.annotations.NotNull;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpEntity;
@@ -54,141 +53,6 @@ public class TradingService {
                 }).getBody();
     }
 
-    public OrderResponse orderCoin() throws IOException, NoSuchAlgorithmException {
-        List<AccountResponse> account = getAccount();
-
-        AccountResponse KRW = new AccountResponse();
-        AccountResponse ETH = new AccountResponse();
-
-        for (AccountResponse accountResponse : account) {
-            if (accountResponse.getCurrency().equals("KRW")) KRW = accountResponse;
-            else if (accountResponse.getCurrency().equals("ETH")) ETH = accountResponse;
-        }
-
-        ObjectMapper objectMapper = new ObjectMapper();
-        JsonNode jsonNode = objectMapper.readTree(aiDecision());
-
-        String decision = jsonNode.path("decision").asText();
-        String side;
-        if (decision.equals("buy")) {
-            side = "bid";
-        } else if (decision.equals("sell")) {
-            side = "ask";
-        } else {
-            return null;
-        }
-
-        double balance = Math.floor(Double.parseDouble(KRW.getBalance()) * 0.9995);
-        String price = Double.toString(balance);
-        String volume = ETH.getBalance();
-        String ord_type = side.equals("bid") ? "price" : "market";
-
-        HashMap<String, String> params = new HashMap<>();
-        params.put("market", "KRW-ETH");
-        params.put("side", side);
-        params.put("ord_type", ord_type);
-
-        if (side.equals("bid")) {
-            params.put("price", price);
-        } else {
-            params.put("volume", volume);
-        }
-
-        String orderUrl = serverUrl + "/v1/orders";
-        HttpHeaders headers = new HttpHeaders();
-        headers.set("Content-Type", "application/json");
-        headers.set("Authorization", jwtTokenProvider.createOrderToken(params));
-        HttpEntity<String> entity = new HttpEntity<>(new Gson().toJson(params), headers);
-
-        return restTemplate.exchange(
-                orderUrl,
-                HttpMethod.POST,
-                entity,
-                new ParameterizedTypeReference<OrderResponse>() {
-                }).getBody();
-    }
-
-    public String aiDecision() throws IOException {
-        final String gptUrl = "https://api.openai.com/v1/chat/completions";
-
-        OkHttpClient client = new OkHttpClient();
-        ObjectMapper objectMapper = new ObjectMapper();
-
-        String extractedData = candleExtract();  // 이 데이터는 JSON 배열 형태입니다.
-
-        // extractedData를 안전하게 JSON으로 포맷
-        ObjectNode requestBodyNode = objectMapper.createObjectNode();
-        requestBodyNode.put("model", "gpt-3.5-turbo");
-        requestBodyNode.put("temperature", 1);
-        requestBodyNode.put("max_tokens", 2048);
-        requestBodyNode.put("top_p", 1);
-        requestBodyNode.put("frequency_penalty", 0);
-        requestBodyNode.put("presence_penalty", 0);
-
-        ArrayNode messages = requestBodyNode.putArray("messages");
-        ObjectNode systemMessage = messages.addObject();
-        systemMessage.put("role", "system");
-        systemMessage.put("content", "You're a Coin Invest expert. Tell me if I should buy, sell, or hold based on the chart data provided. Respond in JSON format. Answer in Korean.\n\nResponse Example:\n{\"decision\": \"buy\", \"reason\": \"기술적인 이유 설명\"}\n{\"decision\": \"sell\", \"reason\": \"기술적인 이유 설명\"}\n{\"decision\": \"hold\", \"reason\": \"기술적인 이유 설명\"}");
-
-
-        ObjectNode userMessage = messages.addObject();
-        userMessage.put("role", "user");
-        userMessage.put("content", extractedData);  // 그대로 이 데이터를 JSON 형식으로 삽입
-
-        // HTTP 요청 생성
-        Request request = new Request.Builder()
-                .url(gptUrl)
-                .addHeader("Authorization", "Bearer " + openAiKey)
-                .addHeader("Content-Type", "application/json")
-                .post(RequestBody.create(requestBodyNode.toString(), MediaType.get("application/json")))
-                .build();
-
-        // 요청 실행 및 응답 처리
-        try (Response response = client.newCall(request).execute()) {
-            if (!response.isSuccessful()) {
-                throw new IOException("Unexpected response code: " + response.code() + " " + response.body().string());
-            }
-
-            // JSON 응답 파싱
-            JsonNode jsonResponse = objectMapper.readTree(response.body().string());
-
-            // 필요한 데이터 추출
-            JsonNode choices = jsonResponse.path("choices");
-            if (choices.isArray() && !choices.isEmpty()) {
-                JsonNode decision = choices.get(0).path("message").path("content");
-                return decision.asText();
-            } else {
-                throw new IOException("No choices in the response.");
-            }
-        }
-    }
-
-    @NotNull
-    private static String candleExtract() throws IOException {
-        // 업비트 차트 전달 15일치 일봉데이터 가져오기
-        OkHttpClient client = new OkHttpClient();
-
-        Request request = new Request.Builder()
-                .url("https://api.upbit.com/v1/candles/days?market=KRW-ETH&count=15")
-                .get()
-                .addHeader("accept", "application/json")
-                .build();
-
-        // try-with-resources 구문을 사용하여 자동으로 닫히도록 함
-        try (Response response = client.newCall(request).execute()) {
-            String jsonResponse = response.body().string();
-
-            // Jackson을 이용한 JSON 파싱
-            ObjectMapper objectMapper = new ObjectMapper();
-            UpbitCandle[] candles = objectMapper.readValue(jsonResponse, UpbitCandle[].class);
-            SimpleCandleDTO[] filteredCandles = Arrays.stream(candles)
-                    .map(candle -> new SimpleCandleDTO(candle.getCandleDateTimeKst(), candle.getOpeningPrice(), candle.getHighPrice(), candle.getLowPrice(), candle.getTradePrice(), candle.getCandleAccTradeVolume()))
-                    .toArray(SimpleCandleDTO[]::new);
-
-            return objectMapper.writeValueAsString(filteredCandles);
-        }
-    }
-
     public void startProgram() throws IOException, InterruptedException, NoSuchAlgorithmException {
         double todayTarget = checkTarget(); // 당일 목표가
         boolean op_mode = false; // 시작하는 날 매수하지않기
@@ -220,12 +84,12 @@ public class TradingService {
 
             Thread.sleep(1000);
 
-            System.out.printf("현재시간: %s%n 목표가: %s%n 현재가: %s%n 보유상태: %s%n 동작상태: %s%n",
-                    now, todayTarget, current, hold, op_mode);
+//            System.out.printf("현재시간: %s 목표가: %s 현재가: %s 보유상태: %s 동작상태: %s%n",
+//                    now, todayTarget, current, hold, op_mode);
         }
     }
 
-    public OrderResponse orderCoins(String decision) throws IOException, NoSuchAlgorithmException {
+    private OrderResponse orderCoins(String decision) throws IOException, NoSuchAlgorithmException {
         List<AccountResponse> account = getAccount();
 
         AccountResponse KRW = new AccountResponse();
@@ -314,7 +178,7 @@ public class TradingService {
         }
     }
 
-    public Double current() throws IOException {
+    private Double current() throws IOException {
         OkHttpClient client = new OkHttpClient();
 
         Request request = new Request.Builder()
@@ -337,4 +201,139 @@ public class TradingService {
             return jsonNode.get(0).get("trade_price").asDouble();
         }
     }
+
+//    public OrderResponse orderCoin() throws IOException, NoSuchAlgorithmException {
+//        List<AccountResponse> account = getAccount();
+//
+//        AccountResponse KRW = new AccountResponse();
+//        AccountResponse ETH = new AccountResponse();
+//
+//        for (AccountResponse accountResponse : account) {
+//            if (accountResponse.getCurrency().equals("KRW")) KRW = accountResponse;
+//            else if (accountResponse.getCurrency().equals("ETH")) ETH = accountResponse;
+//        }
+//
+//        ObjectMapper objectMapper = new ObjectMapper();
+//        JsonNode jsonNode = objectMapper.readTree(aiDecision());
+//
+//        String decision = jsonNode.path("decision").asText();
+//        String side;
+//        if (decision.equals("buy")) {
+//            side = "bid";
+//        } else if (decision.equals("sell")) {
+//            side = "ask";
+//        } else {
+//            return null;
+//        }
+//
+//        double balance = Math.floor(Double.parseDouble(KRW.getBalance()) * 0.9995);
+//        String price = Double.toString(balance);
+//        String volume = ETH.getBalance();
+//        String ord_type = side.equals("bid") ? "price" : "market";
+//
+//        HashMap<String, String> params = new HashMap<>();
+//        params.put("market", "KRW-ETH");
+//        params.put("side", side);
+//        params.put("ord_type", ord_type);
+//
+//        if (side.equals("bid")) {
+//            params.put("price", price);
+//        } else {
+//            params.put("volume", volume);
+//        }
+//
+//        String orderUrl = serverUrl + "/v1/orders";
+//        HttpHeaders headers = new HttpHeaders();
+//        headers.set("Content-Type", "application/json");
+//        headers.set("Authorization", jwtTokenProvider.createOrderToken(params));
+//        HttpEntity<String> entity = new HttpEntity<>(new Gson().toJson(params), headers);
+//
+//        return restTemplate.exchange(
+//                orderUrl,
+//                HttpMethod.POST,
+//                entity,
+//                new ParameterizedTypeReference<OrderResponse>() {
+//                }).getBody();
+//    }
+//
+//    public String aiDecision() throws IOException {
+//        final String gptUrl = "https://api.openai.com/v1/chat/completions";
+//
+//        OkHttpClient client = new OkHttpClient();
+//        ObjectMapper objectMapper = new ObjectMapper();
+//
+//        String extractedData = candleExtract();  // 이 데이터는 JSON 배열 형태입니다.
+//
+//        // extractedData를 안전하게 JSON으로 포맷
+//        ObjectNode requestBodyNode = objectMapper.createObjectNode();
+//        requestBodyNode.put("model", "gpt-3.5-turbo");
+//        requestBodyNode.put("temperature", 1);
+//        requestBodyNode.put("max_tokens", 2048);
+//        requestBodyNode.put("top_p", 1);
+//        requestBodyNode.put("frequency_penalty", 0);
+//        requestBodyNode.put("presence_penalty", 0);
+//
+//        ArrayNode messages = requestBodyNode.putArray("messages");
+//        ObjectNode systemMessage = messages.addObject();
+//        systemMessage.put("role", "system");
+//        systemMessage.put("content", "You're a Coin Invest expert. Tell me if I should buy, sell, or hold based on the chart data provided. Respond in JSON format. Answer in Korean.\n\nResponse Example:\n{\"decision\": \"buy\", \"reason\": \"기술적인 이유 설명\"}\n{\"decision\": \"sell\", \"reason\": \"기술적인 이유 설명\"}\n{\"decision\": \"hold\", \"reason\": \"기술적인 이유 설명\"}");
+//
+//
+//        ObjectNode userMessage = messages.addObject();
+//        userMessage.put("role", "user");
+//        userMessage.put("content", extractedData);  // 그대로 이 데이터를 JSON 형식으로 삽입
+//
+//        // HTTP 요청 생성
+//        Request request = new Request.Builder()
+//                .url(gptUrl)
+//                .addHeader("Authorization", "Bearer " + openAiKey)
+//                .addHeader("Content-Type", "application/json")
+//                .post(RequestBody.create(requestBodyNode.toString(), MediaType.get("application/json")))
+//                .build();
+//
+//        // 요청 실행 및 응답 처리
+//        try (Response response = client.newCall(request).execute()) {
+//            if (!response.isSuccessful()) {
+//                throw new IOException("Unexpected response code: " + response.code() + " " + response.body().string());
+//            }
+//
+//            // JSON 응답 파싱
+//            JsonNode jsonResponse = objectMapper.readTree(response.body().string());
+//
+//            // 필요한 데이터 추출
+//            JsonNode choices = jsonResponse.path("choices");
+//            if (choices.isArray() && !choices.isEmpty()) {
+//                JsonNode decision = choices.get(0).path("message").path("content");
+//                return decision.asText();
+//            } else {
+//                throw new IOException("No choices in the response.");
+//            }
+//        }
+//    }
+//
+//    @NotNull
+//    private static String candleExtract() throws IOException {
+//        // 업비트 차트 전달 15일치 일봉데이터 가져오기
+//        OkHttpClient client = new OkHttpClient();
+//
+//        Request request = new Request.Builder()
+//                .url("https://api.upbit.com/v1/candles/days?market=KRW-ETH&count=15")
+//                .get()
+//                .addHeader("accept", "application/json")
+//                .build();
+//
+//        // try-with-resources 구문을 사용하여 자동으로 닫히도록 함
+//        try (Response response = client.newCall(request).execute()) {
+//            String jsonResponse = response.body().string();
+//
+//            // Jackson을 이용한 JSON 파싱
+//            ObjectMapper objectMapper = new ObjectMapper();
+//            UpbitCandle[] candles = objectMapper.readValue(jsonResponse, UpbitCandle[].class);
+//            SimpleCandleDTO[] filteredCandles = Arrays.stream(candles)
+//                    .map(candle -> new SimpleCandleDTO(candle.getCandleDateTimeKst(), candle.getOpeningPrice(), candle.getHighPrice(), candle.getLowPrice(), candle.getTradePrice(), candle.getCandleAccTradeVolume()))
+//                    .toArray(SimpleCandleDTO[]::new);
+//
+//            return objectMapper.writeValueAsString(filteredCandles);
+//        }
+//    }
 }
