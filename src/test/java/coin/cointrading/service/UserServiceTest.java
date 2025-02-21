11eb2344b8b65b9b6f7b7cc29ea1a55e18529853
KangDongJoon <ehdwns1119@naver.com>
2025-02-22 01:27:23 +0900

    [TEST] UserService 테스트 코드 작성 - 회원가입 실패_중복 아이디 - 로그인 실패_가입하지 않은 아이디 - 로그인 실패_비밀번호 틀림

--- src/test/java/coin/cointrading/service/UserServiceTest.java
+++ src/test/java/coin/cointrading/service/UserServiceTest.java
@@ -1,10 +1,12 @@
 package coin.cointrading.service;
 
 import coin.cointrading.domain.User;
+import coin.cointrading.dto.LoginRequest;
 import coin.cointrading.dto.UserSignupRequest;
 import coin.cointrading.exception.CustomException;
 import coin.cointrading.repository.UserRepository;
 import coin.cointrading.util.AES256Util;
+import coin.cointrading.util.JwtTokenProvider;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.ArgumentCaptor;
@@ -13,6 +15,7 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import java.util.Objects;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,15 +33,19 @@ class UserServiceTest {
     @Mock
     private AES256Util aes256Util;
 
+    @Mock
+    private JwtTokenProvider jwtTokenProvider;
+
     @InjectMocks
     private UserService userService;
 
     @BeforeEach
     void setUp() {
-        MockitoAnnotations.openMocks(this); // Mockito 어노테이션 초기화
+        MockitoAnnotations.openMocks(this);
     }
+
     @Test
-    void signup() throws Exception {
+    void signup_success() throws Exception {
         // given
         UserSignupRequest request = new UserSignupRequest("test1", "test1password", "nick1", "secret", "access");
 
@@ -63,7 +70,7 @@ class UserServiceTest {
     }
 
     @Test
-    void duplicatedId() {
+    void signup_fail_duplicatedId() {
         // given
         UserSignupRequest request = new UserSignupRequest("test1", "test1password", "nick1", "secret1", "access1");
         User user1 = new User();
@@ -72,5 +79,50 @@ class UserServiceTest {
         // when, then
         CustomException exception = assertThrows(CustomException.class, () -> userService.signup(request));
         assertThat(exception.getMessage()).isEqualTo("이미 가입된 ID입니다.");
+    }
+
+    @Test
+    void login_success() {
+        // given
+        LoginRequest request = new LoginRequest("test1", "test1password");
+        User user = new User("test1", "test1password", "nick1", "secret1", "access1");
+        when(userRepository.findByUserId(request.getUserId())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(request.getPassword(), user.getPassword())).thenReturn(true);
+        String testToken = "loginToken";
+        when(jwtTokenProvider.createLoginToken(
+                user.getUserId(),
+                user.getUserNickname(),
+                user.getUpbitSecretKey(),
+                user.getUpbitAccessKey()
+        )).thenReturn(testToken);
+        // when
+        String loginToken = userService.login(request);
+
+        // then
+        assertThat(loginToken).isEqualTo(testToken);
+    }
+
+    @Test
+    void login_fail_id_not_found() {
+        // given
+        LoginRequest request = new LoginRequest("test1", "test1password");
+        when(userRepository.findByUserId(request.getUserId())).thenReturn(Optional.empty());
+
+        // when, then
+        CustomException exception = assertThrows(CustomException.class, () -> userService.login(request));
+        assertThat(exception.getMessage()).isEqualTo("존재하지않는 ID입니다.");
+    }
+
+    @Test
+    void login_fail_unmatched_password() {
+        // given
+        LoginRequest request = new LoginRequest("test1", "failpassword");
+        User user = new User("test1", "test1password", "nick1", "secret1", "access1");
+        when(userRepository.findByUserId(request.getUserId())).thenReturn(Optional.of(user));
+        when(passwordEncoder.matches(request.getPassword(), user.getPassword())).thenReturn(Objects.equals(request.getPassword(), user.getPassword()));
+
+        // when, then
+        CustomException exception = assertThrows(CustomException.class, () -> userService.login(request));
+        assertThat(exception.getMessage()).isEqualTo("비밀번호가 틀렸습니다.");
     }
 }
